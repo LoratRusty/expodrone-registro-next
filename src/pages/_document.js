50e095d1fd9acae21d502ef7b86b705fa5787366
José De Sousa <d.solutionsat@gmail.com>
2024-10-30 00:09:27 -0400

    prueba de deploy 2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,12 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+                    rel="stylesheet"
+                    integrity="sha384-...integrity_hash..."
+                    crossOrigin="anonymous"
+                />
+                <link
                     href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css"
                     rel="stylesheet"
                 />
@@ -14,14 +20,17 @@ export default function Document() {
                 <Main />
                 <NextScript />
                 <script
+                    async
                     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
                     integrity="sha384-...integrity_hash..."
                     crossOrigin="anonymous"
                 ></script>
                 <script
+                    async
                     src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"
                 ></script>
                 <script
+                    async
                     src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"
                 ></script>
             </body>
