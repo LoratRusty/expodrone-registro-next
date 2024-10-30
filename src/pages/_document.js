867551584e047af30fad02fbbe94c953197c67d4
José De Sousa <d.solutionsat@gmail.com>
2024-10-30 13:35:55 -0400

    hj

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es"> {/* Cambiado a "es" si el sitio está en español */}
       <Head>
+        {/* Metaetiqueta viewport para dispositivos móviles */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Estilos de DataTables */}
         <link
           rel="stylesheet"
           href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css"
@@ -13,6 +17,8 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css"
         />
+
+        {/* Scripts de jQuery y DataTables */}
         <script
           src="https://code.jquery.com/jquery-3.6.0.min.js"
           integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl7/IV0XtN1Vh6X56mMv25y8f8OCogZl5U1Uyh3y5t"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
 // src/pages/index.js
+import Head from 'next/head';
 import Formulario from '../components/Formulario';
 import TablaCupos from '../components/TablaCupos';
-import TablaInscritos from '../components/TablaInscritos'; // Importa TablaInscritos
+import TablaInscritos from '../components/TablaInscritos';
 
 export default function Home() {
     return (
-        <div>
+        <div className="main-container">
+            {/* Agrega el título y viewport con el componente Head */}
+            <Head>
+                <title>Formulario de Registro</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Página de registro para conferencias" />
+            </Head>
+
             <Formulario />
             <TablaCupos />
-            {/* Tabla de Usuarios Registrados */}
             <div style={{ marginTop: '2rem' }}>
                 <TablaInscritos />
             </div>
